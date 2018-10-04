0be097f647980dd0aa4ba4543d3ea50a6274898e
sxHelloWorld <sxm9899@rit.edu>
2018-10-04 18:21:45 -0400

    Test out mysql connection

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask
+import pymysql
+
 app = Flask(__name__)
+
+#app.config['MYSQL_DATABASE_DB'] = 'ws_db'
+#app.config['MYSQL_DATABASE_HOST'] = 'ws-db.cxn6r23mlloe.us-east-1.rds.amazonaws.com'
 
 @app.route("/")
 def main():
@@ -7,6 +12,14 @@ def main():
 
 @app.route("/test")
 def test():
+    user = 'wsuser'
+    password = ''
+    conn = pymysql.connect(host='ws-db.cxn6r23mlloe.us-east-1.rds.amazonaws.com',user=user, password=password, db='corpus')
+    cursor = conn.cursor()
+    cursor.execute("SELECT COUNT(*) FROM new_table_python")
+    data = cursor.fetchall()
+    if len(data) != 0:
+        return str(data[0])
     return "Hello docker!"
 
 if __name__ == "__main__":
