05503150a753cea77268c3198c4b4324f9d088ff
Adam Spindler <xmeadamx@gmail.com>
2018-10-06 18:30:04 -0400

    Add gui prototype to web app

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,7 @@ FROM python:3.7-slim
 ADD . /code
 WORKDIR /code
 
-RUN apt-get update
-RUN apt-get install -y build-essential
-
 RUN pip install -r requirements.txt
-RUN make all
 
 EXPOSE 80
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 import pymysql
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static', static_folder='www')
+app.jinja_env.add_extension('pypugjs.ext.jinja.PyPugJSExtension')
 
 #app.config['MYSQL_DATABASE_DB'] = 'ws_db'
 #app.config['MYSQL_DATABASE_HOST'] = 'ws-db.cxn6r23mlloe.us-east-1.rds.amazonaws.com'
@@ -9,6 +10,10 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     return "Hello docker!"
+
+@app.route("/prototype")
+def prototype():
+    return render_template('index.pug')
 
 @app.route("/test")
 def test():
