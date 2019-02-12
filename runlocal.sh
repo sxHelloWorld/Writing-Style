aaa21c47b37436a5929ab37b7b738a51464ab2a4
sxHelloWorld <sxm9899@rit.edu>
2019-02-12 11:21:29 -0500

    Modified runlocal to ignore delete container error if the container is nonexistent

--- runlocal.sh
+++ runlocal.sh
@@ -7,7 +7,7 @@ containerName=ws-container
 docker build -t $imageName -f Dockerfile  .
 
 echo Delete old container...
-docker rm -f $containerName
+docker rm -f $containerName || true
 
 echo Run new container...
 docker run -d -p 80:80 --name $containerName $imageName
